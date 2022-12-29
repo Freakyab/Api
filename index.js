aff40596f8f335d22eba54d7001c8cab0d5e417f
Freakyab <aryanbhisikar1@gmail.com>
2022-12-29 19:13:19 +0530

    api testing 11

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const mongo = require("./api/mongo");
 const PORT = process.env.PORT || 5000;
 const app = express();
-const cors = require("cors");
 // const { MongoClient } = require("mongodb");
 // const router = express.Router();
 
