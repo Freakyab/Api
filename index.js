2c2b63cbbea364e2a60d3734ae02dec5b25726eb
Freakyab <aryanbhisikar1@gmail.com>
2022-12-29 17:10:56 +0530

    api testing 1

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const express = require("express");
         }
       });
       
-      app.use("/", router);
+      app.use("/api", router);
       
       app.listen(port, () => {
         console.log(`Example app listening on port ${port}`);
