1bea0dd3e4272b8909aefcf116e2155c9fa14e72
Freakyab <aryanbhisikar1@gmail.com>
2022-12-31 11:49:06 +0530

    api testing 16

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const mongo = require("./api/mongo");
+const ProjectData = require("./api/ProjectData");
 const PORT = process.env.PORT || 5000;
 const app = express();
-// const { MongoClient } = require("mongodb");
-// const router = express.Router();
-
-// const client = new MongoClient(
-//   "mongodb+srv://DynamicA:D0j3iO5c23I9Lmbo@cluster0.wxp0mkv.mongodb.net/?retryWrites=true&w=majority"
-// );
-
-// router.get("/", async (req, res) => {
-
-//   try {
-//     await client.connect();
-//     const db = client.db("PassDb");
-//     const collection = await db.collection("Pass").aggregate().toArray();
-//     console.log(collection[0].name);
-//     res.json(collection[0].name);
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).send("Server error");
-//   }
-// });
 
 app.use(cors());
 app.use("/api/mongo", mongo);
+app.use("/api/ProjectData", ProjectData);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
