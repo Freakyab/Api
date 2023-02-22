150d88a83bb68a32059d10267f7acfd420867e94
Freakyab <aryanbhisikar1@gmail.com>
2023-02-22 18:40:09 +0530

    updates

--- Linkedin/post.js
+++ Linkedin/post.js
@@ -1,5 +1,5 @@
 // Require modules
-const { MongoClient,ObjectId } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const express = require("express");
 const router = express.Router();
 require("dotenv").config();
@@ -13,10 +13,11 @@ const client = new MongoClient(process.env.DB_URL_LI, {
 router.get("/", async (req, res) => {
     try {
         const userId = req.query.userId;
+        const view = req.query.view;
         await client.connect();
         const db = client.db("LinkedCopy");
         const Collection = await db.collection("UserData");
-        const collection =await db.collection("UserData").aggregate().toArray();
+        const collection = await db.collection("UserData").aggregate().toArray();
 
 
         collection.map((e) => {
@@ -25,16 +26,21 @@ router.get("/", async (req, res) => {
                 console.log(e.post);
             }
         });
-        Collection.updateMany(
-            { _id: ObjectId(userId) },
-            { $set: { post: Post + 1 } }
-        );
-
-        res.json({ status: true,post : Post + 1 });
+        if (view != 0) {
+            Collection.updateMany(
+                { _id: ObjectId(userId) },
+                { $set: { post: Post + 1 } }
+            );
+        }
+        if (view == 0) {
+            res.json({ status: true, post: Post });
+        } else {
+            res.json({ status: true, post: Post + 1 });
+        }
 
     } catch (error) {
         console.error(error);
-    } 
+    }
 });
 
 module.exports = router;
