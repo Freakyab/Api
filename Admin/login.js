cc7a9dbaa4fd714cdb8e1224abbca5a2b0989e75
Freakyab <aryanbhisikar1@gmail.com>
2023-03-28 17:41:50 +0530

    Admin api

---
+++ Admin/login.js
@@ -0,0 +1,49 @@
+// Require modules
+const { MongoClient } = require("mongodb");
+const express = require("express");
+const router = express.Router();
+require("dotenv").config();
+
+//connect to mongodb database
+const client = new MongoClient(process.env.DB_URL_LI, {
+    useUnifiedTopology: true,
+});
+
+router.get("/", async (req, res) => {
+    try {
+        var username = req.query.username;
+        var password = req.query.password;
+
+        var flag = false;
+        var userId;
+
+        //connect to database
+        await client.connect();
+        const db = client.db("PassDb");
+
+        const collection = await db.collection("Pass").aggregate().toArray();
+
+        collection.find((e) => {
+            if (e.username === username) {
+                if (e.password === password) {
+                    flag = true;
+                    userId = e._id;
+
+                }
+            }
+        });
+        if (flag) {
+            res.json({ status: true, userId: userId });
+        }
+        else {
+            res.json({ status: false });
+        }
+
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Server error");
+    }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const mongo = require("./api/mongo");
-const ProjectData = require("./api/ProjectData");
+const adminLogin = require("./Admin/login");
+// const ProjectData = require("./api/ProjectData");
 
 // LinkedinCopy
 const login = require("./Linkedin/login");
@@ -26,8 +26,8 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // app.use(cors());
-app.use("/api/mongo", mongo);
-app.use("/api/ProjectData", ProjectData);
+app.use("/Admin/login", adminLogin);
+// app.use("/api/ProjectData", ProjectData);
 
 // LinkedinCopy
 app.use("/Linkedin/login", login);
