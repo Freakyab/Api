65d4d6461e5cdbe7e9bfda811fb128540e6ab006
Freakyab <aryanbhisikar1@gmail.com>
2023-05-15 20:50:14 +0530

    online done

--- index.js
+++ index.js
@@ -1,66 +1,51 @@
-const express = require("express");
-const cors = require("cors");
-const adminLogin = require("./Admin/login");
-const ProjectData = require("./Admin/ProjectData");
-const sendData = require("./Admin/sendData");
-const feedback = require("./Admin/feedback");
+const io = require('socket.io')(5000, {
+  cors: {
+    origin: '*',
+  }
+});
+const users = {};
 
-// LinkedinCopy
-const login = require("./Linkedin/login");
-const signup = require("./Linkedin/signup")
-const post = require("./Linkedin/post");
-const returnData = require("./Linkedin/return")
-const SpecificPost = require("./Linkedin/specificPost")
-const deletePost = require("./Linkedin/delete")
-const like = require("./Linkedin/like")
-const feed = require("./Linkedin/feed")
 
-// /Testing
-const Login = require("./TestingData/Login");
-const Signup = require("./TestingData/Signup");
-const Post = require("./TestingData/Post");
-const DeletePost = require("./TestingData/DeletePost");
-const Like = require("./TestingData/Like");
-const ProfileFeed = require("./TestingData/ProfileFeed");
-const Feed = require("./TestingData/Feed");
-const share = require("./TestingData/share")
+io.on('connection', socket => {
+  socket.on('new-user', (name, id) => {
+    users[id] = name;
+    socket.broadcast.emit('receive-message', { message: `${name} has joined the chat`, type: 'Name' });
+    socket.broadcast.emit('online', { message: Object.values(users) });
+  });
 
-// Spotify 
-const Spotify = require("./spotify/sendData");
+  socket.on('receive-message', (message, name) => {
+    socket.broadcast.emit('receive-message', { message: message, name: name, type: 'Message' });
+  });
 
-const PORT = process.env.PORT || 5000;
-const app = express();
+  socket.on('leave', (id) => {
+    if (id === undefined) return;
+    else {
+      const name = users[id];
+      delete users[id];
+      socket.broadcast.emit('receive-message', { message: `${name} has left the chat`, type: 'Name' });
+      socket.broadcast.emit('online', { message: Object.values(users) });
+    }
+  });
 
-app.use(cors());
-app.use(express.json());
-app.use("/Admin/login", adminLogin);
-app.use("/Admin/ProjectData", ProjectData);
-app.use("/Admin/sendData", sendData);
-app.use("/Admin/feedback", feedback);
+  socket.on('online', () => {
+    const onlineUsers = Object.values(users);
+    console.log(users);
+    if (onlineUsers.length === 0) {
+      socket.broadcast.emit('online', { message: ["No one is online"] });
+    } else {
+      socket.broadcast.emit('online', { message: onlineUsers });
+    }
+  });
 
-// LinkedinCopy
-app.use("/Linkedin/login", login);
-app.use("/linkedin/signup", signup);
-app.use("/linkedin/post", post);
-app.use("/linkedin/returnData", returnData);
-app.use("/linkedin/specificPost", SpecificPost);
-app.use("/linkedin/deletePost", deletePost);
-app.use("/linkedin/like", like)
-app.use("/linkedin/feed", feed)
-
-// Testing
-app.use("/Testing/Login", Login);
-app.use("/Testing/Signup", Signup);
-app.use("/Testing/Post", Post);
-app.use("/Testing/DeletePost", DeletePost);
-app.use("/Testing/Like", Like);
-app.use("/Testing/ProfileFeed", ProfileFeed);
-app.use("/Testing/Feed", Feed); 
-app.use("/Testing/share", share); 
-
-// Spotify
-app.use("/Spotify", Spotify);
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  socket.on('join-room', (room,name) => {
+    console.log(name,room);
+    socket.join(room);
+    socket.broadcast.to(room).emit('receive-message', { message: `${name} has joined the room`, type: 'Name' });
+    socket.broadcast.to(room).emit('online', { message: Object.values(users) });
+  });
+  socket.on('room-message', (message, name,room) => {
+    console.log(message);
+    socket.broadcast.to(room).emit('room-message', { message: message });
+  }
+  );
 });
