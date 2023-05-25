e730bc31fa42a3fef8481a691e3a0d410cff46ee
Freakyab <aryanbhisikar1@gmail.com>
2023-05-25 11:49:38 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,51 +1,52 @@
-const io = require('socket.io')(5000, {
-  cors: {
-    origin: '*',
+const express = require('express');
+const cors = require('cors');
+const {MongoClient} = require('mongodb');
+require('dotenv').config();
+
+const client = new MongoClient(process.env.DB_URL, {
+  useUnifiedTopology: true,
+});
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+
+app.post('/', async(req, res) => {
+  try{
+    await client.connect();
+    const db = client.db('sample_geospatial');
+    const collection = db.collection('shipwrecks');
+
+    //  Collection insert Titanic;
+    collection.insertOne({name: 'Titanic'})
+
+    // Collection update Titanic to Titanic1;
+    // const Collection = await  collection.updateOne({name: 'Titanic'}, {$set: {name: 'Titanic1'}})
+
+    // Collection Find Titanic1;
+    // const Collection = await collection.find({name : "Titanic1"}).toArray();
+
+    // Collection FindOne titanic1;
+    // const findOne = await collection.findOne({name : "Titanic1"});
+    // console.log(findOne);
+
+    // Collection Delete Titanic1;
+    // const delete_data = await collection.deleteOne({name : "Titanic1"});
+    // console.log(delete_data);
+    
+  // console.log(updateAdd_data);
+    // await collection.updateMany({feature_type : "Wrecks - Submerged, dangerous"}, {$set: {feature_type: "Wrecks - Submerged, dangerous1"}})
+    // const Collection = await collection.find({
+    //   feature_type : "Wrecks - Submerged, dangerous"
+    // }).toArray();
+    // await collection.deleteMany({feature_type : "Wrecks - Submerged, dangerous1"})
+    // const Collection = await collection.find({feature_type : "Wrecks - Submerged, dangerous1"}).toArray();
+  }catch(err){
+    console.log(err);
   }
 });
-const users = {};
 
 
-io.on('connection', socket => {
-  socket.on('new-user', (name, id) => {
-    users[id] = name;
-    socket.broadcast.emit('receive-message', { message: `${name} has joined the chat`, type: 'Name' });
-    socket.broadcast.emit('online', { message: Object.values(users) });
-  });
-
-  socket.on('receive-message', (message, name) => {
-    socket.broadcast.emit('receive-message', { message: message, name: name, type: 'Message' });
-  });
-
-  socket.on('leave', (id) => {
-    if (id === undefined) return;
-    else {
-      const name = users[id];
-      delete users[id];
-      socket.broadcast.emit('receive-message', { message: `${name} has left the chat`, type: 'Name' });
-      socket.broadcast.emit('online', { message: Object.values(users) });
-    }
-  });
-
-  socket.on('online', () => {
-    const onlineUsers = Object.values(users);
-    console.log(users);
-    if (onlineUsers.length === 0) {
-      socket.broadcast.emit('online', { message: ["No one is online"] });
-    } else {
-      socket.broadcast.emit('online', { message: onlineUsers });
-    }
-  });
-
-  socket.on('join-room', (room,name) => {
-    console.log(name,room);
-    socket.join(room);
-    socket.broadcast.to(room).emit('receive-message', { message: `${name} has joined the room`, type: 'Name' });
-    socket.broadcast.to(room).emit('online', { message: Object.values(users) });
-  });
-  socket.on('room-message', (message, name,room) => {
-    console.log(message);
-    socket.broadcast.to(room).emit('room-message', { message: message });
-  }
-  );
-});
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
