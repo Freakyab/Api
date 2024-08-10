f612128b0d03b38017663ab903d92cc96008e299
Freakyab <aryanbhisikar1@gmail.com>
2024-08-10 15:55:31 +0530

    chore: Update image uploader route and controller to sort images by most recent

--- imageUploader/controller.js
+++ imageUploader/controller.js
@@ -19,14 +19,62 @@ router.get("/get/:limit", async (req, res) => {
     const db = client.db("imageSaver");
     const collection = db.collection("ImageUploader");
 
-    // Find and limit the results, then skip the previous records
-    const images = await collection.find().skip(skipValue).limit(2).toArray();
+    // Find and sort by _id in descending order to get the most recent images first
+    const images = await collection
+      .find()
+      .sort({ _id: -1 }) // Sort by _id in descending order (most recent first)
+      .skip(skipValue)
+      .limit(2)
+      .toArray();
 
     // Return the images as a response
     return res.status(200).json(images);
   } catch (error) {
     console.error(error);
-    return res.status(500).send("Server error");
+    return res.status(500).json({ message: "Server error" });
+  } finally {
+    await client.close(); // Ensure the client is closed after the request is handled
+  }
+});
+
+router.post("/upload", async (req, res) => {
+  try {
+    // Connect to database
+    const { image, link } = req.body;
+    console.log(image);
+    await client.connect();
+    const db = client.db("imageSaver");
+    const collection = db.collection("ImageUploader");
+
+    // Find and limit the results, then skip the previous records
+    const doc = await collection.insertOne({
+      image,
+      link,
+      visibility: false,
+    });
+    if (doc.insertedId) {
+      return res.status(200).json({
+        message: "Image uploaded successfully",
+        status: true,
+        doc : {
+          _id : doc.insertedId,
+          image,
+          link,
+          visibility: false
+        }
+      });
+    } else {
+      return res.status(400).json({
+        message: "Image not uploaded",
+        status: false,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Server error",
+      status: false,
+    });
   } finally {
     await client.close(); // Ensure the client is closed after the request is handled
   }
--- index.js
+++ index.js
@@ -45,7 +45,9 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 app.use("/Admin/login", adminLogin);
 app.use("/Admin/ProjectData", ProjectData);
 app.use("/Admin/sendData", sendData);
